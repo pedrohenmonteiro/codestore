476318e89271600e0ce108501b34b686a1198461
Pedro Monteiro <pedromont436@gmail.com>
2023-12-05 01:14:07 -0300

    primeiro commit

---
+++ src/test/java/com/pedromonteiro/codestore/CodestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedromonteiro.codestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
